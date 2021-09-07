a0670fefaae9ec832bd37a56c06e57dfc12843b7
Ricardo <ricardo.treumun@gmail.com>
2021-09-06 23:24:39 -0300

    se agregan cambios relacionados con subir la foto

--- src/pages/places/PlacesComments.js
+++ src/pages/places/PlacesComments.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Text,
   View,
@@ -9,9 +9,83 @@ import {
   ImageBackground,
   TouchableOpacity,
 } from 'react-native';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
 export default function PlacesComments() {
-    return (
-      <Text> COMENTARIOS !!  </Text>
-    )
+  const [imageUri, setImageUri] = useState();
+
+  const updatePhotoServer = image_uri => {
+    let subirData = new FormData();
+    subirData.append('submit', 'ok');
+    subirData.append('name', 'Ricardo');
+    subirData.append('comment', 'estes es un comentario');
+    subirData.append('valor1', '1');
+    subirData.append('photo', {
+      type: 'image/jpg',
+      uri: image_uri,
+      name: 'updateimagentmp.jpg',
+    });
+
+    fetch('http://192.168.0.9:7612/formulario', {
+      method: 'POST',
+      body: subirData,
+    });
+
+  };
+
+  return (
+    <View>
+      <Text> COMENTARIOS !! </Text>
+
+      <TouchableOpacity
+        onPress={() =>
+          launchCamera(
+            {cameraType: 'back', mediaType: 'photo', saveToPhotos: true},
+            response => {
+              if (response.didCancel) {
+                return;
+              }
+              if (!response.assets[0].uri) {
+                return;
+              }
+              console.log(response.assets[0]);
+              console.log(response.assets[0].data);
+              setImageUri(response.assets[0].uri);
+              updatePhotoServer(response.assets[0].uri);
+            },
+          )
+        }
+        style={{
+          backgroundColor: 'white',
+          width: 130,
+          padding: 10,
+          margin: 15,
+          borderRadius: 5,
+        }}>
+        <Text style={{textAlign: 'center'}}>Usar cámara</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() =>
+          launchImageLibrary({mediaType: 'photo'}, response => {
+            if (response.didCancel) {
+              return;
+            }
+            if (!response.assets[0].uri) {
+              return;
+            }
+            setImageUri(response.assets[0].uri);
+          })
+        }
+        style={{
+          backgroundColor: 'white',
+          width: 130,
+          padding: 10,
+          margin: 15,
+          borderRadius: 5,
+        }}>
+        <Text style={{textAlign: 'center'}}>Usar Galeria</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
