847a531aa9885b2a73fbe53a7ccc32cc86f0d556
Ricardo <ricardo.treumun@gmail.com>
2021-09-14 00:41:15 -0300

    nuevos cambios

--- src/pages/places/PlacesComments.js
+++ src/pages/places/PlacesComments.js
@@ -19,6 +19,9 @@ export default function PlacesComments() {
   const context = useContext(AppContext);
   const {dataApp, setDataApp} = context;
 
+  const [name, setName] = useState();
+  const [commentario, setComentario] = useState();
+
   const updatePhotoServer = image_uri => {
     let subirData = new FormData();
     subirData.append('idPlaces', '1');
@@ -35,16 +38,76 @@ export default function PlacesComments() {
       method: 'POST',
       body: subirData,
     });
+  };
+
+  const subirComentario = () => {
+    console.log(name);
+    console.log(commentario);
+    console.log(imageUri);
+
+    let subirData = new FormData();
+    subirData.append('idPlaces', dataApp.placeSelect);
+    subirData.append('name', name);
+    subirData.append('comment', commentario);
+    subirData.append('photo', {
+      type: 'image/jpg',
+      uri: imageUri,
+      name: 'updateimagentmp.jpg',
+    });
+
+    fetch('http://192.168.0.6:7612/formulario', {
+      method: 'POST',
+      body: subirData,
+    });
 
   };
 
   return (
     <View>
-      <Text> COMENTARIOS !!  </Text>
+      <Text> COMENTARIOS !! </Text>
 
-      <Text> ID TIPO: {dataApp.typePlacesSelect}  </Text>
-      <Text> ID LUGAR: {dataApp.placeSelect}  </Text>
- 
+      <Text> ID TIPO: {dataApp.typePlacesSelect} </Text>
+      <Text> ID LUGAR: {dataApp.placeSelect} </Text>
+
+      <View>
+        <TextInput
+          onChangeText={setName}
+          value={Text}
+          placeholder="Ingresa tu nombre"
+          keyboardType="text"
+        />
+
+        <TextInput
+          onChangeText={setComentario}
+          value={Text}
+          placeholder="Ingresa tu comentario"
+          keyboardType="text"
+        />
+
+        <Button
+          onPress={() =>
+            launchCamera(
+              {cameraType: 'back', mediaType: 'photo', saveToPhotos: true},
+              response => {
+                if (response.didCancel) {
+                  return;
+                }
+                if (!response.assets[0].uri) {
+                  return;
+                }
+                console.log(response.assets[0]);
+                console.log(response.assets[0].data);
+                setImageUri(response.assets[0].uri);
+                updatePhotoServer(response.assets[0].uri);
+              },
+            )
+          }
+          title={'subir foto'}
+        />
+
+        <Button onPress={() => subirComentario()} title={'Comenta0r'} />
+      </View>
+
       <TouchableOpacity
         onPress={() =>
           launchCamera(
